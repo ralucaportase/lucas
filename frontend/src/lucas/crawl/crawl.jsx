5e6df686c623d1a106984b192727b2ec12a1fc60
Teodor Voicencu <teodorvoicencu@gmail.com>
2020-08-13 23:55:20 +0300

    Make Lucas more styled

--- frontend/src/lucas/crawl/crawl.jsx
+++ frontend/src/lucas/crawl/crawl.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import LucasInput from './lucasInput';
@@ -7,36 +6,22 @@ import CrawlButton from './crawlButton';
 import CrawlTable from './crawlTable';
 import AlertError from './alertError';
 import LoadCrawlResults from './loadCrawlResults';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-    button: {
-        height: '100%',
-    },
-}));
+import styles from './styles/crawl.scss';
 
 const Crawl = () => {
     const [url, setUrl] = React.useState('http://quotes.toscrape.com/');
 
-    const classes = useStyles();
     return (
         <>
             <LoadCrawlResults />
-            <div className={classes.root}>
-                <Paper className={classes.paper}>
+            <div className={styles.root}>
+                <Paper className={styles.paper}>
                     <Grid container spacing={2}>
                         <Grid item xs={10}>
                             <LucasInput value={url} onChange={(url) => setUrl(url)} label={'URL'} />
                         </Grid>
                         <Grid item xs={2}>
-                            <CrawlButton className={classes.button} url={url} />
+                            <CrawlButton className={styles.button} url={url} />
                         </Grid>
                     </Grid>
                 </Paper>
